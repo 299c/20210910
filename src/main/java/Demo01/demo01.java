24452a531f3e8d1ac4e820fecb5ae849f2cf5062
c299 <2839659142@qq.com>
2021-09-17 23:55:07 +0800

    添加了JDBC的基础演示

---
+++ src/main/java/Demo01/demo01.java
@@ -0,0 +1,56 @@
+package Demo01;
+
+import java.sql.*;
+
+public class demo01 {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        // 1 注册驱动
+        Class.forName("com.mysql.jdbc.Driver");
+        // 2 获取连接
+        Connection connection = DriverManager.getConnection("jdbc:mysql:///test01","root","root");
+        // 3 准备传输器
+        Statement statement = connection.createStatement();
+        // 4 执行sql
+        ResultSet resultSet = statement.executeQuery("select * from book");
+        String sql1="insert into book (id,name,author,prices) values(null,'我的暑假生活','小明',50)";
+        // 5 获取结果
+        while (resultSet.next()) {
+            System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2)
+                    + resultSet.getString(3) + " " + resultSet.getDouble(4)
+            );
+        }
+        // 6 关闭资源
+        resultSet.close();
+        statement.close();
+        connection.close();
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
